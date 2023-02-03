fc34e12176a3f8170522e28f9a2edd259fc3bdee
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-03 22:09:32 +0100

    Modified some things

---
+++ gateway-mic/src/test/java/org/coder/gatewaymic/GatewayMicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coder.gatewaymic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
