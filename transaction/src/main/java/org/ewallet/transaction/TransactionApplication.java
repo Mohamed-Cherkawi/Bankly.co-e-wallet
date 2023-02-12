759a0777f44e2ae2398551ce40a13b32017aa1fc
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-12 20:05:23 +0100

    Exposed the package where the wallet apis are to the enableFeignClients Anno

--- transaction/src/main/java/org/ewallet/transaction/TransactionApplication.java
+++ transaction/src/main/java/org/ewallet/transaction/TransactionApplication.java
@@ -3,9 +3,11 @@ package org.ewallet.transaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients("org.ewallet.transaction.client.apis")
 public class TransactionApplication {
 
 	public static void main(String[] args) {
