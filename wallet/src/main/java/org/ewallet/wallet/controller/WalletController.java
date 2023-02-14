58b23612183b8648c0c030f9bb24a62e2229c715
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-14 16:21:58 +0100

    Add two methods the getSingleWalletByOwnerReferenceApi and updateWalletBalanceApi

--- wallet/src/main/java/org/ewallet/wallet/controller/WalletController.java
+++ wallet/src/main/java/org/ewallet/wallet/controller/WalletController.java
@@ -1,12 +1,12 @@
 package org.ewallet.wallet.controller;
 
-
 import lombok.RequiredArgsConstructor;
 import org.ewallet.wallet.dto.WalletDto;
 import org.ewallet.wallet.service.interfaces.WalletServiceInter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,17 +29,30 @@ public class WalletController{
                 ? ResponseEntity.ok(walletDtos)
                 : ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-    @GetMapping("/fetching/single/ref/{uuid}")
+    @GetMapping("/fetching/single/ref/uuid/{uuid}")
     ResponseEntity<Object> getSingleWalletByReferenceApi(@PathVariable("uuid") String reference){
         WalletDto wallet = walletService.getWalletByUuid(reference);
 
-        return (wallet != null) ? ResponseEntity.ok(wallet) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no wallet with this refrence");
+        return (wallet != null) ? ResponseEntity.ok(wallet) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no wallet with this uuid reference");
+    }
+    @GetMapping("/fetching/single/ref/owner/{ownerId}")
+    ResponseEntity<Object> getSingleWalletByOwnerReferenceApi(@PathVariable("ownerId") String reference){
+        WalletDto wallet = walletService.getWalletByOwnerReference(reference);
+
+        return (wallet != null) ? ResponseEntity.ok(wallet) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no wallet with this owner reference");
     }
     @PostMapping("/adding")
     ResponseEntity<Object> saveWalletApi(@RequestBody WalletDto walletDto){
         WalletDto wallet = walletService.createWallet(walletDto);
         return (wallet != null)
                 ? ResponseEntity.ok(wallet)
-                : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong please retry");
+                : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("The Wallet wasn't saved something went wrong please retry");
+    }
+    @PatchMapping("/updating/balance")
+    ResponseEntity<Object> updateWalletBalanceApi(@RequestBody WalletDto walletDto){
+        WalletDto wallet = walletService.updateWallet(walletDto);
+        return (wallet != null)
+                ? ResponseEntity.ok(wallet)
+                : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("The Wallet wasn't updated please try again");
     }
 }
