50d27ecaf8630712a3f366d29bda8228c5269f73
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-14 16:16:53 +0100

    Write the methods to be used by the service implementation

--- transaction/src/main/java/org/ewallet/transaction/service/interfaces/TransactionServiceInter.java
+++ transaction/src/main/java/org/ewallet/transaction/service/interfaces/TransactionServiceInter.java
@@ -4,6 +4,5 @@ import org.ewallet.transaction.dto.TransactionDto;
 
 public interface TransactionServiceInter {
     TransactionDto createTransaction(TransactionDto transactionDto);
-    void deposit();
-    void withdrawal();
+    String makeOperation(String operationType, TransactionDto walletDto);
 }
