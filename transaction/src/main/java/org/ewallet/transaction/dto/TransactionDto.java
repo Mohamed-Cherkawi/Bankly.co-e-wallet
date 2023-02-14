a6dc958cdc82e9e214bb1b01f3a149cce7774965
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-14 16:12:55 +0100

    Add some annotations

--- transaction/src/main/java/org/ewallet/transaction/dto/TransactionDto.java
+++ transaction/src/main/java/org/ewallet/transaction/dto/TransactionDto.java
@@ -1,12 +1,18 @@
 package org.ewallet.transaction.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
 
 @ToString @Setter @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionDto implements Serializable {
     private String uuid;
     private String type;
