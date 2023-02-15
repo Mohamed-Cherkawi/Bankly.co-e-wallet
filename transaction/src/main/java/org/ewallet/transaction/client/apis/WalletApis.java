2da3746fb8e93ed48538cb4e0d1c7d22560855dc
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-15 11:51:07 +0100

    Changed PATCH to PUT because feign does not support the patch method yet

--- transaction/src/main/java/org/ewallet/transaction/client/apis/WalletApis.java
+++ transaction/src/main/java/org/ewallet/transaction/client/apis/WalletApis.java
@@ -4,23 +4,23 @@ import org.ewallet.transaction.dto.WalletDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @FeignClient(name = "ewallet-wallet-service-v1")
 public interface WalletApis {
-    @GetMapping("/wallet/api/v1/fetching/all")
+    @GetMapping("wallet/api/v1/fetching/all")
     ResponseEntity<List<WalletDto>> getAllWalletsApi();
-    @GetMapping("/wallet/api/v1/fetching/single/ref/uuid/{uuid}")
+    @GetMapping("wallet/api/v1/fetching/single/ref/uuid/{uuid}")
     ResponseEntity<Object> getSingleWalletByReferenceApi(@PathVariable("uuid") String reference);
-    @GetMapping("/wallet/api/v1/fetching/single/ref/owner/{ownerId}")
+    @GetMapping("wallet/api/v1/fetching/single/ref/owner/{ownerId}")
     ResponseEntity<Object> getSingleWalletByOwnerReferenceApi(@PathVariable("ownerId") String reference);
-    @PostMapping("/wallet/api/v1/adding")
+    @PostMapping("wallet/api/v1/adding")
     ResponseEntity<Object> saveWalletApi(@RequestBody WalletDto walletDto);
-    @PatchMapping("/wallet/api/v1/updating/balance")
+    @PutMapping("wallet/api/v1/updating/balance")
     ResponseEntity<WalletDto> updateWalletBalanceApi(@RequestBody WalletDto walletDto);
 }
