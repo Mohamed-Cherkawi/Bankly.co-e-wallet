32a9cde1a15b3116d36b9e8936b6c9ebd7b0ab1e
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-02-17 09:25:52 +0100

    Removed the @EnableDiscoveryClient because in these recent versions bringing the eureka client dependencies is enough , eureka register theses services by default to discovery server

--- microservices-holder/transaction/src/main/java/org/ewallet/transaction/TransactionApplication.java
+++ microservices-holder/transaction/src/main/java/org/ewallet/transaction/TransactionApplication.java
@@ -2,12 +2,10 @@ package org.ewallet.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("org.ewallet.transaction.client.apis")
-@EnableDiscoveryClient
 public class TransactionApplication {
 
 	public static void main(String[] args) {
--- microservices-holder/wallet/src/main/java/org/ewallet/wallet/WalletApplication.java
+++ microservices-holder/wallet/src/main/java/org/ewallet/wallet/WalletApplication.java
@@ -2,10 +2,8 @@ package org.ewallet.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class WalletApplication {
 
 	public static void main(String[] args) {
